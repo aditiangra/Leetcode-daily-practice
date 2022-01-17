de4119aef104507417c786960678ef8917d54ffa
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-01-17 15:59:57 +0530

    Time: 32 ms (38.28%), Space: 13.5 MB (81.36%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-      int n=graph.size();
-        vector<int>color(n,-1);
-        queue<int>q;
-        for(int i=0;i<n;i++)
-        {
-            if(color[i]=-1)
-            {
-                if(check(i,graph,color)==false)return false;
-            }
-        }
-        return true;
+    bool isBipartite(vector<vector<int>>& adj) {
+        int V=adj.size();
+         vector<int>color(V,-1);
+	    for(int i=0;i<V;i++)
+	    {
+	        if(color[i]==-1)
+	        {
+                color[i]=1;
+	            if(check(adj,color,i)==false)return false;
+	        }
+	    }
+	    return true;
     }
-    bool check(int src,vector<vector<int>>& graph,vector<int>color)
-    {
-        queue<int>q;
-        q.push(src);
-        color[src]=1;
-        while(q.empty()==false)
-        {
-            int node=q.front();
-            q.pop();
-            for(auto it:graph[node])
-            {
-                if(color[it]==-1)
-                {
-                    color[it]=1-color[node];
-                    q.push(it);
-                }
-                else if(color[it]==color[node])
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    	bool check( vector<vector<int>>& adj, vector<int>&color,int src)
+	{
+	  
+	    for(auto it:adj[src])
+	    {
+	        if(color[it]==-1)
+	        {
+	            color[it]=1-color[src];
+                if(check(adj,color,it)==false)return false;
+	        }
+	        else if(color[it]==color[src])return false;
+	  
+	      
+	    }
+              return true;
+	}
 };
