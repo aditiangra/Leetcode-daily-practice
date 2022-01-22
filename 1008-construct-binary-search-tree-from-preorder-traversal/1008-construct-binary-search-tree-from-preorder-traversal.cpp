a98c68547a5428d57fa51ae9f181f13aae41694f
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-01-22 17:56:16 +0530

    Time: 10 ms (23.16%), Space: 12.5 MB (96.30%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,35 +12,39 @@
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int n=preorder.size();
-        vector<int>inorder(n);
-        for(int i=0;i<n;i++)
+       if(preorder.size()==0)return nullptr;
+        TreeNode* root=new TreeNode(preorder[0]);
+        for(int i=1;i<preorder.size();i++)
         {
-            inorder[i]=preorder[i];
+            insertIntoBst(root,preorder[i]);
         }
-        sort(inorder.begin(),inorder.end());
-        TreeNode* root=buildTree(preorder,inorder);
         return root;
     }
-  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int,int>mpp;
-        for(int i=0;i<inorder.size();i++)
-        {
-            mpp[inorder[i]]=i;
-        }
-        TreeNode* root=build(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mpp);
-        return root;            
-    }
-    
-    TreeNode* build(vector<int>&preorder,int prestart,int preend,vector<int>&inorder,int instart,int inend,map<int,int>&mpp)
+    void insertIntoBst(TreeNode* root,int val)
     {
-        if(prestart>preend or instart>inend)return nullptr;
-        TreeNode* root=new TreeNode(preorder[prestart]);
-        int inroot=mpp[root->val];
-        int numsleft=inroot-instart;
-        root->left=build(preorder,prestart+1,prestart+numsleft,inorder,instart,inroot-1,mpp);
-        root->right=build(preorder,prestart+numsleft+1,preend,inorder,inroot+1,inend,mpp);
-        return root;
-    
+        if(root==NULL)return ;
+        TreeNode* curr=root;
+        while(true)
+        {
+            if(curr->val<=val)
+            {
+                if(curr->right!=nullptr)curr=curr->right;
+                 else
+            {
+                curr->right=new TreeNode(val);
+                return;
+            }
+            }
+            else
+            {
+                if(curr->left!=nullptr)curr=curr->left;
+                else
+                {
+                    curr->left=new TreeNode(val);
+                    return;
+                }
+            }
+           
+        }
     }
 };
