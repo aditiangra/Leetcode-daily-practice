bfb8c53373fba273efce9e918a84e6819a5aae93
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-01-24 14:17:11 +0530

    Time: 212 ms (17.60%), Space: 96.2 MB (83.70%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+      int ans=0;
+        int i,j;
+        int row=grid.size();
+        int col=grid[0].size();
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(grid[i][j]==1){
+                ans+=4;
+                if(i&&grid[i-1][j])ans--;
+                if(j&&grid[i][j-1])ans--;
+                if(i<row-1 &&grid[i+1][j])ans--;
+                if(j<col-1&&grid[i][j+1])ans--;}
+            }
+        }
+        return ans;
+    }
+};
