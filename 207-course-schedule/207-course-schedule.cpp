df46208511823ba11ff7a8bae9719046f137da21
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-03-05 12:01:25 +0530

    Time: 31 ms (53.99%), Space: 13.8 MB (61.39%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& pre) {
-        if(V==0)return true;
-          vector<vector<int>> adj(V);
-        for(int i=0; i<pre.size(); i++) // convert edge list to adjacency list
-            adj[pre[i][1]].push_back(pre[i][0]);
-        vector<int>indegree(V,0);
+    bool canFinish(int V, vector<vector<int>>&pre) {
+        vector<int> adj[V];
+        for(auto it:pre)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>indegree(V);
+        for(int i=0;i<V;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
         queue<int>q;
         for(int i=0;i<V;i++)
         {
-            for(auto it:adj[i])indegree[it]++;
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
         }
-        //indegree done//
-        for(int i=0;i<indegree.size();i++)
-        {
-            if(indegree[i]==0)q.push(i);
-        }
-        //pushing done//
-        int count=0;
+        int cntr=0;
         while(q.empty()==false)
         {
-            int node=q.front();
+            int u=q.front();
             q.pop();
-            count++;
-            for(auto it:adj[node])
+            for(auto it:adj[u])
             {
                 indegree[it]--;
                 if(indegree[it]==0)q.push(it);
             }
+            cntr++;
         }
-        if(count==V)return true;
-        return false;
+        if(cntr==V)return true;
+      
+            return false;
     }
 };
