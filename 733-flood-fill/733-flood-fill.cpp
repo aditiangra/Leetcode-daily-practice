bc602bf12dc72576f54cb07acfb170a63e809984
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-03-10 15:55:26 +0530

    Time: 15 ms (44.58%), Space: 14.2 MB (37.92%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-     bool isValid(vector<vector<int>>&image,int i,int j,int n,int m,int color,int newColor)
-    {
-        if(i>=0 && i<n && j>=0 && j<m && image[i][j]==color)return true;
-        return false;
+   bool isValid(vector<vector<int> > & arr,int i,int j,int el){
+        if(i<0 or j<0 or i>=arr.size() or j>=arr[0].size() or el!=arr[i][j])return false;
+       return true;
     }
-      void floodFill(vector<vector<int>>&image,int i,int j,int n,int m,int color,int newColor)
-    {
-        image[i][j]=newColor;
-        if(isValid(image,i+1,j,n,m,color,newColor)==true)
-        {
-            floodFill(image,i+1,j,n,m,color,newColor);
-        }
-         if(isValid(image,i-1,j,n,m,color,newColor)==true)
-        {
-            floodFill(image,i-1,j,n,m,color,newColor);
-        }
-         if(isValid(image,i,j+1,n,m,color,newColor)==true)
-        {
-            floodFill(image,i,j+1,n,m,color,newColor);
-        }
-         if(isValid(image,i,j-1,n,m,color,newColor)==true)
-        {
-            floodFill(image,i,j-1,n,m,color,newColor);
+    
+    
+    
+    void bfs(int sr,int sc,int k,int ele,vector< vector<int> > & arr){
+        queue<pair<int,int> > q;
+        q.push(make_pair(sr,sc));
+        while( q.empty()==false){
+            auto current=q.front();
+            q.pop();
+            int i=current.first,j=current.second;
+            arr[i][j]=k;
+            if(isValid(arr,i-1,j,ele)) q.push(make_pair(i-1,j));
+            if(isValid(arr,i,j-1,ele)) q.push(make_pair(i,j-1));
+            if(isValid(arr,i+1,j,ele)) q.push(make_pair(i+1,j));
+            if(isValid(arr,i,j+1,ele)) q.push(make_pair(i,j+1));
         }
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int n=image.size();
-        int m=image[0].size();
-        int color=image[sr][sc];
-        if(color==newColor)return image;
-        floodFill(image,sr,sc,n,m,color,newColor);
+
+    
+
+
+    void dfs(int i,int j,int k,int ele,vector< vector<int> > & arr){
+        if(i<0 or j<0 or i>=arr.size() or j>=arr[0].size() or arr[i][j]!=ele) return;
+        arr[i][j]=k;
+        dfs(i-1,j,k,ele,arr);
+        dfs(i,j-1,k,ele,arr);
+        dfs(i+1,j,k,ele,arr);
+        dfs(i,j+1,k,ele,arr);
+    }
+    vector<vector<int> > floodFill(vector<vector<int> >& image, int sr, int sc, int newColor) {
+        int k=newColor;
+        int ele=image[sr][sc];
+        if (ele != newColor)
+            // dfs(sr,sc,newColor,image[sr][sc],image);
+            bfs(sr,sc,k,ele,image);
         return image;
     }
-  
-   
+
+
 };
