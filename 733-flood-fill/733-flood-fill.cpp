33831ddda3f55114e78aba443281d599fd952cd6
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-03-12 12:03:23 +0530

    Time: 14 ms (48.76%), Space: 14.2 MB (28.04%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,46 +1,59 @@
 class Solution {
 public:
-   bool isValid(vector<vector<int> > & arr,int i,int j,int el){
-        if(i<0 or j<0 or i>=arr.size() or j>=arr[0].size() or el!=arr[i][j])return false;
-       return true;
+    void dfs(vector<vector<int>>&image,int i,int j,int oldColor,int newColor,int n,int m,vector<vector<int>>&visited)
+    {
+        if(i<0 or j<0 or i>=n or j>=m)return ;
+        if(visited[i][j]==1 or image[i][j]!=oldColor)return ;
+        visited[i][j]=1;
+        image[i][j]=newColor;
+        dfs(image,i-1,j,oldColor,newColor,n,m,visited);
+         dfs(image,i+1,j,oldColor,newColor,n,m,visited);
+         dfs(image,i,j-1,oldColor,newColor,n,m,visited);
+         dfs(image,i,j+1,oldColor,newColor,n,m,visited);
     }
-    
-    
-    
-    void bfs(int sr,int sc,int k,int ele,vector< vector<int> > & arr){
-        queue<pair<int,int> > q;
-        q.push(make_pair(sr,sc));
-        while( q.empty()==false){
-            auto current=q.front();
-            q.pop();
-            int i=current.first,j=current.second;
-            arr[i][j]=k;
-            if(isValid(arr,i-1,j,ele)) q.push(make_pair(i-1,j));
-            if(isValid(arr,i,j-1,ele)) q.push(make_pair(i,j-1));
-            if(isValid(arr,i+1,j,ele)) q.push(make_pair(i+1,j));
-            if(isValid(arr,i,j+1,ele)) q.push(make_pair(i,j+1));
+    bool isValid(vector<vector<int>>&image,int i,int j,int oldColor,int newColor,int n,int m,vector<vector<int>>&visited)
+    {
+        if(i<0 or j<0 or i>=n or j>=m)return false;
+        if(visited[i][j]==1 or image[i][j]!=oldColor)return false;
+        return true;
+    }
+    void bfs(vector<vector<int>>&image,int i,int j,int oldColor,int newColor,int n,int m,vector<vector<int>>&visited)
+    {
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        visited[i][j]=1;
+        while(q.empty()==false)
+        {
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                auto it=q.front();
+                q.pop();
+                int x=it.first;
+                int y=it.second;
+                if(isValid(image,x-1,y,oldColor,newColor,n,m,visited))
+                {visited[x-1][j]=1;
+                q.push({x-1,y});}
+                  if(isValid(image,x+1,y,oldColor,newColor,n,m,visited))
+                  {visited[x+1][y]=1;
+                      q.push({x+1,y});}
+                  if(isValid(image,x,y-1,oldColor,newColor,n,m,visited))
+                  {visited[x][y-1]=1;
+                      q.push({x,y-1});}
+                  if(isValid(image,x,y+1,oldColor,newColor,n,m,visited))
+                  {visited[x][y+1]=1;
+                      q.push({x,y+1});}
+                
+            }
         }
     }
-
-    
-
-
-    void dfs(int i,int j,int k,int ele,vector< vector<int> > & arr){
-        if(i<0 or j<0 or i>=arr.size() or j>=arr[0].size() or arr[i][j]!=ele) return;
-        arr[i][j]=k;
-        dfs(i-1,j,k,ele,arr);
-        dfs(i,j-1,k,ele,arr);
-        dfs(i+1,j,k,ele,arr);
-        dfs(i,j+1,k,ele,arr);
-    }
-    vector<vector<int> > floodFill(vector<vector<int> >& image, int sr, int sc, int newColor) {
-        int k=newColor;
-        int ele=image[sr][sc];
-        if (ele != newColor)
-            // dfs(sr,sc,newColor,image[sr][sc],image);
-            bfs(sr,sc,k,ele,image);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int x=image.size();
+        int y=image[0].size();
+        vector<vector<int>> visited(x,vector<int>(y,0));
+        int oldColor=image[sr][sc];
+        dfs(image,sr,sc,oldColor,newColor,x,y,visited); 
+        /*bfs(image,sr,sc,oldColor,newColor,x,y,visited);*/
         return image;
     }
-
-
 };
