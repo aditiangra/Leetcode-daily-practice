3867ee7a07d574c9b5e4f3639a28be897b058dc7
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-03-12 20:46:38 +0530

    Time: 17 ms (31.94%), Space: 14 MB (50.56%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,59 +1,68 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&image,int i,int j,int oldColor,int newColor,int n,int m,vector<vector<int>>&visited)
-    {
-        if(i<0 or j<0 or i>=n or j>=m)return ;
-        if(visited[i][j]==1 or image[i][j]!=oldColor)return ;
-        visited[i][j]=1;
-        image[i][j]=newColor;
-        dfs(image,i-1,j,oldColor,newColor,n,m,visited);
-         dfs(image,i+1,j,oldColor,newColor,n,m,visited);
-         dfs(image,i,j-1,oldColor,newColor,n,m,visited);
-         dfs(image,i,j+1,oldColor,newColor,n,m,visited);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int n=image.size();
+        int m=image[0].size();
+        int color=image[sr][sc];
+        if(color!=newColor)
+        {
+            bfs(image,sr,sc,color,newColor,n,m);
+        }
+        return image;
     }
-    bool isValid(vector<vector<int>>&image,int i,int j,int oldColor,int newColor,int n,int m,vector<vector<int>>&visited)
-    {
-        if(i<0 or j<0 or i>=n or j>=m)return false;
-        if(visited[i][j]==1 or image[i][j]!=oldColor)return false;
-        return true;
-    }
-    void bfs(vector<vector<int>>&image,int i,int j,int oldColor,int newColor,int n,int m,vector<vector<int>>&visited)
+    void bfs(vector<vector<int>>& image,int i, int j,int color, int newColor,int n,int m)
     {
         queue<pair<int,int>>q;
         q.push({i,j});
-        visited[i][j]=1;
         while(q.empty()==false)
         {
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {
-                auto it=q.front();
-                q.pop();
-                int x=it.first;
-                int y=it.second;
-                if(isValid(image,x-1,y,oldColor,newColor,n,m,visited))
-                {visited[x-1][j]=1;
-                q.push({x-1,y});}
-                  if(isValid(image,x+1,y,oldColor,newColor,n,m,visited))
-                  {visited[x+1][y]=1;
-                      q.push({x+1,y});}
-                  if(isValid(image,x,y-1,oldColor,newColor,n,m,visited))
-                  {visited[x][y-1]=1;
-                      q.push({x,y-1});}
-                  if(isValid(image,x,y+1,oldColor,newColor,n,m,visited))
-                  {visited[x][y+1]=1;
-                      q.push({x,y+1});}
-                
-            }
+            auto current=q.front();
+            q.pop();
+            int x=current.first;
+            int y=current.second;
+            image[x][y]=newColor;
+            if(isValid(image,x+1,y,color,newColor,n,m))
+        {
+            q.push({x+1,y});
+        }
+         if(isValid(image,x-1,y,color,newColor,n,m))
+        {
+            q.push({x-1,y});
+        }
+         if(isValid(image,x,y+1,color,newColor,n,m))
+        {
+          q.push({x,y+1});
+        }
+         if(isValid(image,x,y-1,color,newColor,n,m))
+        {
+            q.push({x,y-1});
+        }
+        }
+        
+    }
+    void dfs(vector<vector<int>>& image,int i, int j,int color, int newColor,int n,int m)
+    {
+        image[i][j]=newColor;
+        if(isValid(image,i+1,j,color,newColor,n,m))
+        {
+            dfs(image,i+1,j,color,newColor,n,m);
+        }
+         if(isValid(image,i-1,j,color,newColor,n,m))
+        {
+            dfs(image,i-1,j,color,newColor,n,m);
+        }
+         if(isValid(image,i,j+1,color,newColor,n,m))
+        {
+            dfs(image,i,j+1,color,newColor,n,m);
+        }
+         if(isValid(image,i,j-1,color,newColor,n,m))
+        {
+            dfs(image,i,j-1,color,newColor,n,m);
         }
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int x=image.size();
-        int y=image[0].size();
-        vector<vector<int>> visited(x,vector<int>(y,0));
-        int oldColor=image[sr][sc];
-        dfs(image,sr,sc,oldColor,newColor,x,y,visited); 
-        /*bfs(image,sr,sc,oldColor,newColor,x,y,visited);*/
-        return image;
+    bool isValid(vector<vector<int>>& image,int i, int j,int color, int newColor,int n,int m)
+    {
+        if(i>=0 and j>=0 and i<n and j<m and image[i][j]==color)return true;
+        return false;
     }
 };
