495282c0ab6de1a4ed4433818c75c7ae398d74f4
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-04-06 14:15:44 +0530

    Time: 28 ms (98.89%), Space: 12.3 MB (83.94%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int islands=0;
-        for(int i=0;i<grid.size();i++)
+        int n=grid.size();
+        int m=grid[0].size();
+        int count=0;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<grid[0].size();j++)
+            for(int j=0;j<m;j++)
             {
                 if(grid[i][j]=='1')
                 {
-                    dfs(grid,i,j);
-                    ++islands;
+                    count++;
+                    dfs(grid,i,j,n,m);
                 }
             }
         }
-        return islands;
+        return count;
     }
-    void dfs(vector<vector<char>>& grid,int i,int j)
+    bool isValid(vector<vector<char>>& grid,int i,int j,int n,int m)
+    {
+        if(i>=0 and i<n and j>=0 and j<m and grid[i][j]=='1')return true;
+        return false;
+    }
+    void dfs(vector<vector<char>>& grid,int i,int j,int n,int m)
     {
         grid[i][j]='2';
-        if(isValid(grid,i-1,j))dfs(grid,i-1,j);
-        if(isValid(grid,i+1,j))dfs(grid,i+1,j);
-        if(isValid(grid,i,j-1))dfs(grid,i,j-1);
-        if(isValid(grid,i,j+1))dfs(grid,i,j+1);
-    }
-    bool isValid(vector<vector<char>>& grid,int i,int j)
-    {
-        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size())
-            return false;
-        if(grid[i][j]=='2' or grid[i][j]=='0')
-            return false;
-        return true;
+        if(isValid(grid,i-1,j,n,m)==true)
+        {
+            dfs(grid,i-1,j,n,m);
+        }
+        if(isValid(grid,i+1,j,n,m)==true)
+        {
+            dfs(grid,i+1,j,n,m);
+        }
+        if(isValid(grid,i,j-1,n,m)==true)
+        {
+            dfs(grid,i,j-1,n,m);
+        }
+        if(isValid(grid,i,j+1,n,m)==true)
+        {
+            dfs(grid,i,j+1,n,m);
+        }
     }
 };
