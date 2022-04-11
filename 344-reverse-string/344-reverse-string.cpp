d552f51637c72eb02a0ec9d4031db28ffacb8982
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-04-11 16:24:04 +0530

    Time: 50 ms (8.01%), Space: 23.2 MB (41.92%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    void rever(vector<char>& s,int i,int n)
+    {
+        if(i>=n/2)
+        {
+            return ;
+        }
+        swap(s[i],s[n-1-i]);
+        rever(s,i+1,n);
+    }
     void reverseString(vector<char>& s) {
-        stack<char>stk;
-        for(int i=0;i<s.size();i++)
-        {
-            stk.push(s[i]);
-        }
+        int n=s.size();
         int i=0;
-        while(stk.empty()==false)
-        {
-            s[i++]=stk.top();
-            stk.pop();
-        }
+        rever(s,i,n);
     }
 };
