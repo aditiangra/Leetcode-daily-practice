dd8bca4a5b045e3cbc39b7c5d4bd7d37f69c3f3e
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-04-18 11:20:35 +0530

    Time: 29 ms (53.27%), Space: 12.8 MB (98.81%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>&pre) {
+    bool canFinish(int V, vector<vector<int>>& pre) {
         vector<int> adj[V];
-        for(auto it:pre)
+        for(int i=0;i<pre.size();i++)
         {
-            adj[it[1]].push_back(it[0]);
+            adj[pre[i][0]].push_back(pre[i][1]);
         }
-        vector<int>indegree(V);
+        vector<int>indegree(V,0);
         for(int i=0;i<V;i++)
         {
             for(auto it:adj[i])
@@ -22,20 +22,22 @@ public:
                 q.push(i);
             }
         }
-        int cntr=0;
+        int cnt=0;
         while(q.empty()==false)
         {
-            int u=q.front();
+            int node=q.front();
             q.pop();
-            for(auto it:adj[u])
+            for(auto it:adj[node])
             {
                 indegree[it]--;
-                if(indegree[it]==0)q.push(it);
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
             }
-            cntr++;
+            cnt++;
         }
-        if(cntr==V)return true;
-      
-            return false;
+        if(cnt==V)return true;
+        return false;
     }
 };
