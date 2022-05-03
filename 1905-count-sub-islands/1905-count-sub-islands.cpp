415d1408c009564ff777fbd7c4e4a0f265452714
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-03 19:06:22 +0530

    Time: 333 ms (83.37%), Space: 92.3 MB (34.74%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<int>>& grid1,vector<vector<int>>& grid2,int &flag){
-        
-        if(i<0 || j<0 || i>=grid2.size() || j>=grid2[0].size() || grid2[i][j]==0){
-            return;
-        }
-        
-        if(grid1[i][j]!=grid2[i][j]){
-            flag = 0;
-        } 
-        
-        grid2[i][j] = 0;
-        
-        dfs(i-1,j,grid1,grid2,flag);
-        dfs(i,j-1,grid1,grid2,flag);
-        dfs(i+1,j,grid1,grid2,flag);
-        dfs(i,j+1,grid1,grid2,flag);
-        
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int i,int j,int &flag)
+    {
+        if(i<0 or j<0 or i>=grid2.size() or j>=grid2[0].size() or grid2[i][j]==0 )return;
+        if(grid1[i][j]!=grid2[i][j])flag=0;
+        grid2[i][j]=0;
+        dfs(grid1,grid2,i-1,j,flag);
+        dfs(grid1,grid2,i+1,j,flag);
+        dfs(grid1,grid2,i,j-1,flag);
+        dfs(grid1,grid2,i,j+1,flag);
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        
-        int ans=0;
-        int n=grid1.size();
-        int m=grid1[0].size();
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                
-                if(grid2[i][j]==1){
-                    
+      int ans=0;
+
+        for(int i=0;i<grid2.size();i++)
+        {
+            for(int j=0;j<grid2[0].size();j++)
+            {
+                if(grid2[i][j]==1)
+                {
                     int flag=1;
-                    dfs(i,j,grid1,grid2,flag);
-                    if(flag){
-                       ans+=1;
+                    dfs(grid1,grid2,i,j,flag);
+                    if(flag==1)
+                    {
+                        ans++;
                     }
-                    
                 }
             }
         }
-        
         return ans;
     }
-   
 };
