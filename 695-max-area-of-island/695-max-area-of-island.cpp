646542a5d87404e64e3cd634f0093d59bb278592
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-10 11:19:14 +0530

    Time: 21 ms (71.61%), Space: 23.2 MB (65.84%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -3,62 +3,30 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int ans=0;
-        int area=0;
+        int res=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    area=1;
-                    bfs(grid,i,j,n,m,area);
-                    ans=max(ans,area);
+                    int count=0;
+                    dfs(grid,i,j,count);
+                    res=max(res,count);
                 }
             }
         }
-        return ans;
+        return res;
     }
-    bool isValid(vector<vector<int>>& grid,int i,int j,int n,int m,int &area)
+    void dfs(vector<vector<int>>& grid,int i,int j,int &count)
     {
-        if(i>=0 and i<n and j>=0 and j<m and grid[i][j]==1)return true;
-        return false;
-    }
-    void bfs(vector<vector<int>>& grid,int i,int j,int n,int m,int &area)
-    {
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        grid[i][j]=2;
-        while(q.empty()==false)
-        {
-            auto pair=q.front();
-            q.pop();
-            int x=pair.first;
-            int y=pair.second;
-            if(isValid(grid,x-1,y,n,m,area))
-            {
-                area++;
-                grid[x-1][y]=2;
-                q.push({x-1,y});
-            }
-             if(isValid(grid,x+1,y,n,m,area))
-            {
-                area++;
-                 grid[x+1][y]=2;
-                q.push({x+1,y});
-            }
-             if(isValid(grid,x,y-1,n,m,area))
-            {
-                area++;
-                 grid[x][y-1]=2;
-                q.push({x,y-1});
-            }
-             if(isValid(grid,x,y+1,n,m,area))
-            {
-                area++;
-                 grid[x][y+1]=2;
-                q.push({x,y+1});
-            }
-        }
+        if(i<0 or j<0 or i>=grid.size() or j>=grid[0].size() or grid[i][j]==0)
+            return;
+        count++;
+        grid[i][j]=0;
+        dfs(grid,i-1,j,count);
+        dfs(grid,i+1,j,count);
+        dfs(grid,i,j-1,count);
+        dfs(grid,i,j+1,count);
     }
 };
