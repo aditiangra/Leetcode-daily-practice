221d5346f2bd16b45f92cc1743ddb184946fcb4f
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-10 15:58:20 +0530

    Time: 791 ms (7.71%), Space: 10.2 MB (87.41%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    //Brute Force method//
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++)
+        int n=nums.size();
+        vector<int>ans;
+        if(n==0)return ans;
+        for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<nums.size();j++)
+            for(int j=i+1;j<n;j++)
             {
-              if(nums[i]+nums[j]==target)
-              {
-                  ans.push_back(i);
-                  ans.push_back(j);
-              }
+                if(nums[i]+nums[j]==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
             }
         }
         return ans;
     }
-     //vector<int> twoSum(vector<int>& nums, int target) {
-        
-   // }
 };
