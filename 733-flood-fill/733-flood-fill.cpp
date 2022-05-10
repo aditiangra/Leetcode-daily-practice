6b3b72bd293c513a9a020b6b39ed751027f3b2af
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-10 09:30:09 +0530

    Time: 14 ms (45.48%), Space: 13.8 MB (92.09%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         int n=image.size();
         int m=image[0].size();
-       int oldColor=image[sr][sc];
+        int oldColor=image[sr][sc];
         dfs(image,sr,sc,oldColor,newColor);
-        for(int i=0;i<image.size();i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<image[0].size();j++)
+            for(int j=0;j<m;j++)
             {
                 if(image[i][j]==-1)
                 {
@@ -17,32 +17,12 @@ public:
         }
         return image;
     }
-    void dfs(vector<vector<int>>&image,int i,int j,int oldColor,int newColor)
+    void dfs(vector<vector<int>>& image, int sr, int sc, int oldColor,int newColor)
     {
-        image[i][j]=-1;
-        if(isValid(image,i-1,j,oldColor,newColor)==true)
-        {
-            dfs(image,i-1,j,oldColor,newColor);
-        }
-         if(isValid(image,i+1,j,oldColor,newColor)==true)
-        {
-            dfs(image,i+1,j,oldColor,newColor);
-        }
-         if(isValid(image,i,j-1,oldColor,newColor)==true)
-        {
-            dfs(image,i,j-1,oldColor,newColor);
-        }
-         if(isValid(image,i,j+1,oldColor,newColor)==true)
-        {
-            dfs(image,i,j+1,oldColor,newColor);
-        }
-    }
-    bool isValid(vector<vector<int>>&image,int i,int j,int oldColor,int newColor)
-    {
-        if(i>=0 and j>=0 and i<image.size() and j<image[0].size() and image[i][j]==oldColor)
-        {
-            return true;
-        }
-        return false;
+        image[sr][sc]=-1;
+        if(sr-1>=0 and image[sr-1][sc]==oldColor)dfs(image,sr-1,sc,oldColor,newColor);
+        if(sr+1<image.size() and image[sr+1][sc]==oldColor)dfs(image,sr+1,sc,oldColor,newColor);
+        if(sc-1>=0 and image[sr][sc-1]==oldColor)dfs(image,sr,sc-1,oldColor,newColor);
+        if(sc+1<image[0].size() and image[sr][sc+1]==oldColor)dfs(image,sr,sc+1,oldColor,newColor);
     }
 };
