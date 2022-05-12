02e59f696b31f532f7956e3586d1dfebfe93f0e6
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-12 13:02:10 +0530

    Time: 2 ms (41.39%), Space: 6.8 MB (73.06%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -8,42 +8,41 @@ public:
         int dir=1;
         while(left<=right and top<=bottom)
         {
-            if(dir==1){
-            for(int i=left;i<=right;i++)
+            if(dir==1)
             {
-                ans.push_back(matrix[top][i]);
-               
-            }
-                 dir=2;
+                for(int i=left;i<=right;i++)
+                {
+                    ans.push_back(matrix[top][i]);
+                }
                 top++;
+                dir=2;
             }
-            else if(dir==2){
-            for(int i=top;i<=bottom;i++)
+            else if(dir==2)
             {
-                ans.push_back(matrix[i][right]);
-               
-            }
-                 dir=3;
+                for(int i=top;i<=bottom;i++)
+                {
+                    ans.push_back(matrix[i][right]);
+                }
                 right--;
+                dir=3;
             }
             else if(dir==3)
             {
                 for(int i=right;i>=left;i--)
                 {
                     ans.push_back(matrix[bottom][i]);
-                   
                 }
-                 dir=4;
-                    bottom--;
+                bottom--;
+                dir=4;
             }
-            else if(dir==4)
+           else if(dir==4)
             {
                 for(int i=bottom;i>=top;i--)
                 {
                     ans.push_back(matrix[i][left]);
                 }
-                dir=1;
                 left++;
+                dir=1;
             }
         }
         return ans;
