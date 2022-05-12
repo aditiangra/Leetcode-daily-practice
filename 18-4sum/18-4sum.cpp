122b89009fab588dc0bdb2259289dd5f6e44928e
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-12 12:35:59 +0530

    Time: 49 ms (86.35%), Space: 13.3 MB (50.20%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>res;
+        vector<vector<int>>result;
+        if(nums.empty()==true)return result;
         int n=nums.size();
-        if(n==0)return res;
+        int target3,target2,front,back,two_sum;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++)
         {
-            int target3=target-nums[i];
+            target3=target-nums[i];
             for(int j=i+1;j<n;j++)
             {
-                int target2=target3-nums[j];
-                int front=j+1;
-                int back=n-1;
+                target2=target3-nums[j];
+                front=j+1;
+                back=n-1;
                 while(front<back)
                 {
-                    int two_sum=nums[front]+nums[back];
-                    if(target2>two_sum)
+                    two_sum=nums[front]+nums[back];
+                    if(two_sum<target2)
                     {
-                        front++;
+                     front++;   
                     }
-                    else if(target2<two_sum)
+                    else if(two_sum>target2)
                     {
                         back--;
                     }
@@ -31,8 +32,8 @@ public:
                         quad[1]=nums[j];
                         quad[2]=nums[front];
                         quad[3]=nums[back];
-                        res.push_back(quad);
-                        while(front<back and nums[front]==quad[2])++front;
+                        result.push_back(quad);
+                          while(front<back and nums[front]==quad[2])++front;
                         while(front<back and nums[back]==quad[3])--back;
                     }
                 }
@@ -40,6 +41,6 @@ public:
             }
             while(i+1<n and nums[i]==nums[i+1])++i;
         }
-        return res;
+        return result;
     }
 };
