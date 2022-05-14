88cf6fb1477c904c9023d310a1aaa3748f1a3119
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-14 16:10:53 +0530

    Time: 911 ms (5.00%), Space: 92.2 MB (34.10%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int i,int j,int &flag)
-    {
-        if(i<0 or j<0 or i>=grid2.size() or j>=grid2[0].size() or grid2[i][j]==0 )return;
-        if(grid1[i][j]!=grid2[i][j])flag=0;
-        grid2[i][j]=0;
-        dfs(grid1,grid2,i-1,j,flag);
-        dfs(grid1,grid2,i+1,j,flag);
-        dfs(grid1,grid2,i,j-1,flag);
-        dfs(grid1,grid2,i,j+1,flag);
+    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j, bool& res) {
+        if(i < 0 || i >= grid2.size() || j < 0 || j >= grid2[0].size() || grid2[i][j] != 1 )
+            return;
+        if(grid2[i][j]!=grid1[i][j]) {
+            cout << i << " " << j << endl;
+            res = false;
+            return;
+        }
+        grid2[i][j] = -1;
+        dfs(grid1, grid2, i-1, j, res);
+        dfs(grid1, grid2, i+1, j, res);
+        dfs(grid1, grid2, i, j-1, res);
+        dfs(grid1, grid2, i, j+1, res);
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-      int ans=0;
-
-        for(int i=0;i<grid2.size();i++)
-        {
-            for(int j=0;j<grid2[0].size();j++)
-            {
-                if(grid2[i][j]==1)
-                {
-                    int flag=1;
-                    dfs(grid1,grid2,i,j,flag);
-                    if(flag==1)
-                    {
+        int n = grid2.size();
+        int m = grid2[0].size();
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid2[i][j] == 1) {
+                    bool res = true;
+                    dfs(grid1, grid2, i, j, res);
+                    if(res) {
+                        // cout << i << " " << j << endl;
                         ans++;
                     }
                 }
