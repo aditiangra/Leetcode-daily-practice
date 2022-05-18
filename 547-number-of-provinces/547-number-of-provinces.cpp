2d8ef1122e401c44caf2d28124855e5c9c74bbd6
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-18 07:22:03 +0530

    Time: 30 ms (58.50%), Space: 14.9 MB (13.68%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& M) {
-        int n=M.size();
-        if(n==0)return n;
-        int circles=0;
-        vector<bool> visited(n,false);  
-        for(int i=0;i<n;i++){
-            if(visited[i])continue;
-            queue<int> q1;
-            q1.push(i);
-            circles++;
-
-           while(!q1.empty()){               
-           int cur=q1.front();
-           q1.pop();
-           visited[cur]=true;
-               for(int j=0;j<M[cur].size();j++){
-                    if(visited[j]==false&&M[cur][j]==1){
-                    q1.push(j);
-                    visited[j]=true;
-                    }
-               }
-                    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<bool>visited(n,false);
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(visited[i]==false)
+            {
+                dfs(isConnected,visited,i,n);
+                count++;
             }
-              
-            
         }
-        return circles;
+        return count;
+    }
+    void dfs(vector<vector<int>>&isConnected,vector<bool>&visited,int s,int n)
+    {
+        visited[s]=true;
+        vector<int>adj;
+        for(int i=0;i<n;i++)
+        {
+            int x=isConnected[s][i];
+            if(x==1)
+            {
+                adj.push_back(i);
+            }
+        }
+        for(auto x:adj)
+        {
+            if(visited[x]==false)
+            {
+                dfs(isConnected,visited,x,n);
+            }
+        }
     }
 };
