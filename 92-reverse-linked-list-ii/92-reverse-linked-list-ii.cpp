85c471dc9cc525e593260c1f30e1decdd55aa65d
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-24 13:04:01 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (63.72%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -1,54 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
+private:
+    ListNode* successor;
 public:
-    ListNode *listReverse(ListNode *head, ListNode *tailNext) {
-  if (head->next == tailNext) {  //Reached the right indexed node 
-    return head;
-  }
-  
-  ListNode *p = listReverse(head->next, tailNext);
-  head->next->next = head;  //Reverse the link between the head and the head of the reversed list (so far)
-  head->next = tailNext;  //Instead of nullptr, we point it to the node after the right indexed node
-  return p;
-}
-  ListNode* reverseBetween(ListNode* head, int left, int right) {
-  ListNode *leftNode = head;
-  int count = left;
-  ListNode *leftParent = nullptr;  //Keeps track of node, just before left indexed node 
-  
-  //Traverse to left indexed node
-  while (count > 1) {
-    leftParent = leftNode;
-    leftNode = leftNode->next;
-    count--;
-  }        
-  
-  //Traverse to right indexed node
-  count = right;   
-  ListNode *rightNode = head;
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(left == 1)
+            return reverseFirstN(head, right);
         
-  while (count > 1) {
-    rightNode = rightNode->next;
-    count--;
-  }    
-  
-  ListNode* revList = listReverse(leftNode, rightNode->next);
-
-  // If the left index is the beginning of the list, nothing needs to be done 
-  if (!leftParent) {
-    return revList;
-  }
-  //Point the left indexed node's predecessor to the newly reversed list
-  leftParent->next = revList;       
-  return head;        
-}
+        head->next = reverseBetween(head->next, left - 1, right - 1);
+        return head;
+    }
+    
+    ListNode* reverseFirstN(ListNode* head, int firstN) {
+        if(firstN == 1) {
+            successor = head->next;
+            return head;
+        }
+        
+        ListNode* last = reverseFirstN(head->next, firstN - 1);
+        
+        head->next->next = head;
+        head->next = successor;
+        
+        return last;
+    }
 };
