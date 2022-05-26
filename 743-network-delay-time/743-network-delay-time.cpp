838b8bc335af7f9ac798b4e996edc09569142a7f
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-05-26 08:50:33 +0530

    Time: 259 ms (22.12%), Space: 36.6 MB (97.68%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int>dist(n+1,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;//distance,node//
-        vector<vector<int>> adj[n+1];
-        for(int i=0;i<times.size();i++)
+    int networkDelayTime(vector<vector<int>>& times, int V, int K) {
+        int E=times.size();
+        if(E==0)return 0;
+        vector<int>dist(V+1,INT_MAX);
+        dist[K]=0;
+        for(int i=0;i<V;i++)
         {
-            int u=times[i][0];
-            int v=times[i][1];
-            int w=times[i][2];
-            adj[u].push_back({v,w});
-        }
-        dist[k]=0;
-        pq.push({0,k});
-        while(pq.empty()==false)
-        {
-            auto p=pq.top();
-            pq.pop();
-            int currdist=p.first;
-            int currnode=p.second;
-            for(auto it:adj[currnode])
+            for(int j=0;j<E;j++)
             {
-                int nextnode=it[0];//v
-                int weight=it[1];//w
-                if(dist[currnode]+weight<dist[nextnode])
+                int u=times[j][0];
+                int v=times[j][1];
+                int wt=times[j][2];
+                if(dist[u]!=INT_MAX && dist[u]+wt<dist[v])
                 {
-                    dist[nextnode]=currdist+weight;
-                    pq.push({dist[nextnode],nextnode});
+                    dist[v]=dist[u]+wt;
                 }
             }
         }
-       for(int i=1;i<n+1;i++)cout<<dist[i]<<" ";
         int res=0;
-        for(int i=1;i<n+1;i++)
+        for(int i=1;i<=V;i++)
         {
             res=max(res,dist[i]);
         }
         return res==INT_MAX?-1:res;
+        
     }
 };
