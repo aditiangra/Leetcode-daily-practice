37dfa8e24ff4e77930871905613cdc90b92fbdb2
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-06-04 22:20:29 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (30.25%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -7,13 +7,17 @@ public:
     }
     int solve(vector<int>& dp,int n)
     {
-        if(n==0 or n==1)return 1;
+        int prev1=1,prev2=1,curri;
+        
         dp[0]=1;
         dp[1]=1;
       for(int i=2;i<=n;i++)
       {
-          dp[i]=dp[i-1]+dp[i-2];
+          curri=prev1+prev2;
+           prev2=prev1;
+          prev1=curri;
+         
       }
-        return dp[n];
+        return prev1;
     }
 };
