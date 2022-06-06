7867a23a8c69e510cc54536f2586632b862a7ded
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-06-06 21:41:20 +0530

    Time: 21 ms (14.51%), Space: 14 MB (81.14%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    bool isValid(vector<vector<int>>& image,int i,int j,int oldColor,int newColor)
+    {
+        if(i>=0 and j>=0 and i<image.size() and j<image[0].size() and image[i][j]==oldColor)return true;
+        return false;
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         int n=image.size();
         int m=image[0].size();
@@ -15,14 +20,26 @@ public:
                 }
             }
         }
-        return image;
+return image;
     }
-    void dfs(vector<vector<int>>& image, int sr, int sc, int oldColor,int newColor)
+    void dfs(vector<vector<int>>& image,int i,int j,int oldColor,int newColor)
     {
-        image[sr][sc]=-1;
-        if(sr-1>=0 and image[sr-1][sc]==oldColor)dfs(image,sr-1,sc,oldColor,newColor);
-        if(sr+1<image.size() and image[sr+1][sc]==oldColor)dfs(image,sr+1,sc,oldColor,newColor);
-        if(sc-1>=0 and image[sr][sc-1]==oldColor)dfs(image,sr,sc-1,oldColor,newColor);
-        if(sc+1<image[0].size() and image[sr][sc+1]==oldColor)dfs(image,sr,sc+1,oldColor,newColor);
+        image[i][j]=-1;
+        if(isValid(image,i-1,j,oldColor,newColor))
+        {
+            dfs(image,i-1,j,oldColor,newColor);
+        }
+        if(isValid(image,i+1,j,oldColor,newColor))
+        {
+            dfs(image,i+1,j,oldColor,newColor);
+        }
+        if(isValid(image,i,j-1,oldColor,newColor))
+        {
+            dfs(image,i,j-1,oldColor,newColor);
+        }
+        if(isValid(image,i,j+1,oldColor,newColor))
+        {
+            dfs(image,i,j+1,oldColor,newColor);
+        }
     }
 };
