8a90ca22c63748143c7180e3f4856a1152b212dd
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-06-10 12:20:46 +0530

    Time: 19 ms (23.67%), Space: 14.1 MB (51.14%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
-    bool isValid(vector<vector<int>>& image,int i,int j,int oldColor,int newColor)
-    {
-        if(i>=0 and j>=0 and i<image.size() and j<image[0].size() and image[i][j]==oldColor)return true;
-        return false;
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         int n=image.size();
         int m=image[0].size();
         int oldColor=image[sr][sc];
-        dfs(image,sr,sc,oldColor,newColor);
+        dfs(image,sr,sc,oldColor,newColor,n,m);
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -20,26 +15,32 @@ public:
                 }
             }
         }
-return image;
+        return image;
     }
-    void dfs(vector<vector<int>>& image,int i,int j,int oldColor,int newColor)
+    bool isValid(vector<vector<int>>& image,int i,int j,int oldColor,int newColor,int n,int m)
+    {
+        if(i>=0 and j>=0 and i<n and j<m and image[i][j]==oldColor)return true;
+        return false;
+    }
+    void dfs(vector<vector<int>>& image,int i,int j,int oldColor,int newColor,int n,int m)
     {
         image[i][j]=-1;
-        if(isValid(image,i-1,j,oldColor,newColor))
+        if(isValid(image,i-1,j,oldColor,newColor,n,m))
         {
-            dfs(image,i-1,j,oldColor,newColor);
+            dfs(image,i-1,j,oldColor,newColor,n,m);
         }
-        if(isValid(image,i+1,j,oldColor,newColor))
+        if(isValid(image,i+1,j,oldColor,newColor,n,m))
         {
-            dfs(image,i+1,j,oldColor,newColor);
+            dfs(image,i+1,j,oldColor,newColor,n,m);
         }
-        if(isValid(image,i,j-1,oldColor,newColor))
+        if(isValid(image,i,j-1,oldColor,newColor,n,m))
         {
-            dfs(image,i,j-1,oldColor,newColor);
+            dfs(image,i,j-1,oldColor,newColor,n,m);
         }
-        if(isValid(image,i,j+1,oldColor,newColor))
+        if(isValid(image,i,j+1,oldColor,newColor,n,m))
         {
-            dfs(image,i,j+1,oldColor,newColor);
+            dfs(image,i,j+1,oldColor,newColor,n,m);
         }
+        
     }
 };
