9366c52d5406079a0f94c12fc9d833dabf8c859a
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-06-10 17:24:29 +0530

    Time: 2 ms (42.45%), Space: 6 MB (66.23%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-  
     int numTrees(int n) {
-        vector<int>dp(n+1,0);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
+        // Catalan Number can be given by
+        // (2*nCn) / (n+1)
+        long long int ans = 1;
+        for(int i=0 ; i<n ; i++)
         {
-            for(int j=0;j<i;j++)
-            {
-                dp[i]+=dp[j]*dp[i-j-1];
-            }
+            ans *= 2*n-i;
+            ans /= i+1;
         }
-        return dp[n];
+        return ans/(n+1);
     }
 };
