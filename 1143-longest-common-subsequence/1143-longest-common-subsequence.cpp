8aa43bd76e4b21b0f00d5f10f76e1d26db72e035
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-07-01 13:34:25 +0530

    Time: 41 ms (36.98%), Space: 12.9 MB (52.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,45 @@
+//topic:lcs question1//
 class Solution {
 public:
-    //negative base condition hence we will increase the indexes//
     
     int longestCommonSubsequence(string text1, string text2) {
+        //increasing everything by one//
         int n=text1.size();
         int m=text2.size();
-        //ind1-->n
-       // ind2--->m
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        //base case//
-        for(int ind1=0;ind1<n;ind1++)
-        {
-            for(int ind2=0;ind2<m;ind2++)
-            {
-                if(ind1==0 or ind2==0)
-                {
-                    dp[ind1][ind2]=0;
-                }
-            }
-        }
+         vector<vector<int>> dp(n+1,vector<int>(m+1,0));//dp array initiated by one//
+    for(int i=0;i<=n;i++)dp[i][0]=0;
+    for(int j=0;j<=m;j++)dp[0][j]=0;
         for(int ind1=1;ind1<=n;ind1++)
         {
             for(int ind2=1;ind2<=m;ind2++)
             {
-                 if(text1[ind1-1]==text2[ind2-1]) dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
-                 else dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                  if(text1[ind1-1]==text2[ind2-1])  dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                  else dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+            
             }
         }
-        return dp[n][m];
-    }
+    int len=dp[n][m];
+        int i=n;
+        int j=m;
+        int index=len-1;
+        string str="";
+        for(int k=1;k<=len;k++)str+="$";
+        while(i>0 and j>0)
+        {
+             if (text1[i - 1] == text2[j - 1]) {
+             str[index] = text1[i - 1];
+             index--;
+             i--;
+             j--;
+         } 
+            else if (text1[i - 1] > text2[j - 1]) {
+               i--;
+              }
+            else j--;
+        }
+           cout << str;
+        return len;
+        
+        
+  }
 };
