17c9d6bda348f193daf9bb37e70f0e17a14f22ab
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-07-07 15:26:11 +0530

    Time: 18 ms (61.26%), Space: 9.1 MB (38.18%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,10 +3,10 @@ public:
     int f(int i,int j,string &word1,string &word2,vector<vector<int>>&dp)
     {
         //Base Case//
-        if(i<0)return j+1;
-        if(j<0)return i+1;
+        if(i==0)return j;
+        if(j==0)return i;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(word1[i]==word2[j])return dp[i][j]=f(i-1,j-1,word1,word2,dp);
+        if(word1[i-1]==word2[j-1])return dp[i][j]=f(i-1,j-1,word1,word2,dp);
       return dp[i][j]=min(1+f(i,j-1,word1,word2,dp),min(1+f(i-1,j,word1,word2,dp),1+f(i-1,j-1,word1,word2,dp)));
         
         
@@ -14,7 +14,7 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,word1,word2,dp);
     }
 };
