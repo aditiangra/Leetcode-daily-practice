c71f7015b1e48b54fe1e2dbb515a2fcf877f20ec
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-07-10 08:03:35 +0530

    Time: 202 ms (86.51%), Space: 81.6 MB (35.19%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
+    vector<int> nextSmallerElement(vector<int>& heights)
+    {
+        vector<int>nse(heights.size());
+        stack<int>st;
+        for(int i=heights.size()-1;i>=0;i--)
+        {
+            while(st.empty()==false and heights[st.top()]>=heights[i])st.pop();
+            if(i<heights.size())
+            {
+                nse[i]=st.empty()==false?st.top():heights.size();
+            }
+            st.push(i);
+        }
+        return nse;
+    }
+        vector<int> prevSmallerElement(vector<int>& heights)
+    {
+        vector<int>pse(heights.size());
+        stack<int>st;
+        for(int i=0;i<heights.size();i++)
+        {
+            while(st.empty()==false and heights[st.top()]>=heights[i])st.pop();
+            if(i<heights.size())
+            {
+                pse[i]=st.empty()==false?st.top():-1;
+            }
+            st.push(i);
+        }
+        return pse;
+    }
     int largestRectangleArea(vector<int>& heights) {
+        //next smaller element and previous smaller element//
         int n=heights.size();
-        vector<int>nsr(n);
-        vector<int>nsl(n);
-        stack<int> st;
-        for(int i=n-1;i>=0;i--)
-        {
-            while(st.empty()==false and heights[st.top()]>=heights[i])st.pop();
-            if(i<n)
-            {
-                if(st.empty()==false)
-                {
-                    nsr[i]=st.top();
-                }
-                else
-                {
-                    nsr[i]=n;
-                }
-            }
-            st.push(i);
-        }
-        while(st.empty()==false)st.pop();
-           for(int i=0;i<n;i++)
-        {
-            while(st.empty()==false and heights[st.top()]>=heights[i])st.pop();
-            if(i<n)
-            {
-                if(st.empty()==false)
-                {
-                    nsl[i]=st.top();
-                }
-                else
-                {
-                    nsl[i]=-1;
-                }
-            }
-            st.push(i);
-        }
-        int ans=INT_MIN;
+        vector<int>nse=nextSmallerElement(heights);
+       // for(int i=0;i<n;i++)cout<<nse[i]<<" ";
+       // cout<<endl;
+         vector<int>pse=prevSmallerElement(heights);
+        //for(int i=0;i<n;i++)cout<<pse[i]<<" ";
+       // cout<<endl;
+       int ans=INT_MIN;
         for(int i=0;i<n;i++)
         {
-            ans=max(ans,((nsr[i]-nsl[i]-1)*heights[i]));
+            ans=max(ans,(nse[i]-pse[i]-1)*heights[i]);
         }
         return ans;
-        
     }
 };
