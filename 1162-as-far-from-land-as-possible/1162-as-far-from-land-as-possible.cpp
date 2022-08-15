c481304be6b1f9bd4392498bc5fddb2f5623e644
Aditi Angra <91600893+aditiangra@users.noreply.github.com>
2022-08-15 14:47:33 +0530

    Time: 98 ms (64.49%), Space: 19.7 MB (84.55%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,54 +1,62 @@
 class Solution {
 public:
-    bool isValid(vector<vector<int>>& grid,int i,int j)
+    bool isValid(int i,int j,vector<vector<int>>& grid)
     {
         if(i>=0 and j>=0 and i<grid.size() and j<grid[0].size() and grid[i][j]==0)return true;
         return false;
     }
     int maxDistance(vector<vector<int>>& grid) {
+        //0->water//
+        //1->land//
+        //multisource bfs//
         int n=grid.size();
         int m=grid[0].size();
-        queue<pair<int,int>>q;
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==1)q.push({i,j});
+                if(grid[i][j]==1)
+                {
+                    q.push({i,j});
+                }
             }
         }
-        int cnt=0;
+        int ans=0;
         while(q.empty()==false)
         {
-            cnt++;
-            int n=q.size();
-            for(int k=0;k<n;k++)
+            ans++;
+            int s=q.size();
+            while(s--)
             {
                 int x=q.front().first;
                 int y=q.front().second;
                 q.pop();
-                if(isValid(grid,x-1,y))
+                if(isValid(x-1,y,grid))
                 {
-                    q.push({x-1,y});
                     grid[x-1][y]=1;
+                    q.push({x-1,y});
                 }
-                  if(isValid(grid,x+1,y))
+                if(isValid(x+1,y,grid))
                 {
+                    grid[x+1][y]=1;
                     q.push({x+1,y});
-                       grid[x+1][y]=1;
                 }
-                  if(isValid(grid,x,y-1))
+                if(isValid(x,y-1,grid))
                 {
+                    grid[x][y-1]=1;
                     q.push({x,y-1});
-                       grid[x][y-1]=1;
                 }
-                  if(isValid(grid,x,y+1))
+                if(isValid(x,y+1,grid))
                 {
+                    grid[x][y+1]=1;
                     q.push({x,y+1});
-                       grid[x][y+1]=1;
                 }
+                
             }
         }
-        if(cnt>1)return cnt-1;
+        if(ans>1)return ans-1;
         return -1;
+        
     }
 };
